1a49528a55653242b90dcc68d7053c278a36bc57
jaranvil <jaranvil@gmail.com>
2015-08-11 14:45:13 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CraftNanny</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    
+  </head>
+  <body>
+    
+    <div class="row">
+        <div class="large-12 columns top_bar">
+         <span style="font-weight:bold;font-size:36px;color:#1b9bff">
+          
+           CraftNanny
+         </span>
+          <span style="font-weight:bold;font-size:16px;color:#ffffff">
+          
+           Monitor and Control Minecraft Online through ComputerCraft
+         </span>
+        </div>
+ 
+      </div>
+      
+      <div class="row">
+        <div class="large-12 columns thin_bar">
+         
+        </div>
+ 
+      </div>
+
+
+      <div class="row">  
+      <div class="large-3 columns">
+        <p>
+          <div id='cssmenu'>
+            <ul>
+               <li><a href='signin.php'><span>Login</span></a></li>
+               <li><a href='signup.php'><span>Create Account</span></a></li>
+             </ul>
+          </div>
+          <p>
+          <div id='cssmenu'>
+               
+             <ul>  
+               <li class='active'><a href='index.php'><span>Home</span></a></li>
+               <li><a href='setup.php'><span>Setup Instructions</span></a></li>
+               <li><a href='modules.php'><span>In-game Modules</span></a></li>
+               <li><a href='contact.php'><span>Contact</span></a></li>
+               </ul>
+          </div>
+         
+      </div>
+
+        <div class="large-9 columns">
+        <div id="slider">
+          <img src="http://placehold.it/1000x400&text=[ img 1 ]"/>
+        </div>
+        
+        <hr/>
+       
+ 
+      
+  
+      <div class="row">
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>Track Players</h4>
+       </div>
+      
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>Energy and Fluid Levels</h4>
+         </div>
+        
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>Redstone & Rednet Controls</h4>
+        </div>
+      
+        </div>
+         </div>
+        </div>
+ 
+
+
+       
+        
+
+     
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+
+	
+  </body>
+</html>
