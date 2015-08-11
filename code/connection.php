52a04eb526d9711bb9d6203a762f4bf08b3d9aae
jaranvil <jaranvil@gmail.com>
2015-08-11 15:23:35 -0300

    message

--- code/connection.php
+++ code/connection.php
@@ -3,7 +3,6 @@
 //open database connection
 $dbConn = mysql_connect('', '', '')
 	or die(print_r(mysql_error()));
-mysql_select_db('base_logger') or die(print_r(mysql_error()));
+mysql_select_db('') or die(print_r(mysql_error()));
 
-//$dbConn = mysqli_connect("localhost","root","lockview","repost") or die("Error " . mysqli_error($dbConn)); 
 ?>
