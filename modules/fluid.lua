9fc2860825921ec9fd08d721c60751e91fddb9c9
theudas <psodmann@gmail.com>
2015-09-02 16:20:05 +0200

    Updated the Module
    --		- added support for Pressure Pipes
    --		- added support for multiple tanks
    --		- redone a lot of code to make it object oriented
    --		- fixed the visual bars as they were cut off at the left side

--- modules/fluid.lua
+++ modules/fluid.lua
@@ -1,141 +1,207 @@
-        ---------------------------------------------
-        --      Tank module for caftNanny
-        --      by demethan
-        --      www.breakfastcraft.com
-        --	Modified by Theudas
-	--	www.builtbroken.com
-        ---------------------------------------------
+---------------------------------------------
+--		Tank module for caftNanny
+--		by demethan
+--		www.breakfastcraft.com
+--  		www.craftnanny.org
+--		www.builtbroken.com		
+--  		2015 08 12  demethan: 
+--		-fixed modem support
+--		-did some error magement
+-- 		-added visual bar	
+--		02.09.2015 Theudas:
+--		- added support for Pressure Pipes
+--		- added support for multiple tanks
+--		- redone a lot of code to make it object oriented
+--		- fixed the visual bars as they were cut off at the left side
+---------------------------------------------
  
         -- variables
  
         local containers={}
         local version = 1
  
-        local installer = "Q8ah3K9S"
-        local token = '0'
-        local module_name = ''
-        local username = ''
-        local type = ''
- 
-        -- write text to the terminal screen
-        function draw_text_term(x, y, text, text_color, bg_color)
-          term.setTextColor(text_color)
-          term.setBackgroundColor(bg_color)
-          term.setCursorPos(x,y)
-          write(text)
-        end
- 
-        -- draw a line on the terminal screen
-        function draw_line_term(x, y, length, color)
-                term.setBackgroundColor(color)
-                term.setCursorPos(x,y)
-                term.write(string.rep(" ", length))
-        end
- 
-        function bars()
-                draw_line_term(1, 1, 51, colors.lime)
-                draw_line_term(1, 19, 51, colors.lime)
-                draw_text_term(15, 1, 'CraftNanny Fluid Module', colors.gray, colors.lime)
-                draw_text_term(10, 19, 'www.craftnanny.org', colors.gray, colors.lime)
-        end
- 
-        function terminal_screen()
-                term.clear()
-               
-                bars()
-                draw_text_term(1, 2, 'Module: ', colors.lime, colors.black)
-                draw_text_term(10, 2, module_name, colors.white, colors.black)
-                draw_text_term(1, 3, 'Owner: ', colors.lime, colors.black)
-                draw_text_term(8, 3, username, colors.white, colors.black)
-                draw_text_term(1, 4 , string.rep("-", 51), colors.lime, colors.black)
-               
-                draw_text_term(2, 8, "I dont know what to put here...", colors.white, colors.black)
-        end
- 
-        -- retrieves token from local text file
-        function load_config()
-          sr = fs.open("config.txt", "r")
-                token = sr.readLine()
-                module_name = sr.readLine()
-                username = sr.readLine()
-                type = sr.readLine()
-          sr.close()
-        end
- 
-        -- called for new installations and when the scanner needs to be updated
-        function run_installer()
-                if fs.exists("install") then
-                        fs.delete("install")
-                  end
-                  shell.run("pastebin get "..installer.." install")
-                  sleep(1)
-                  shell.run("install")
-        end
+        local installer = "Ja5bQSqT"
+	local token = '0'
+	local module_name = ''
+	local username = ''
+	local type = ''	
+
+
+	-- write text to the terminal screen
+	function draw_text_term(x, y, text, text_color, bg_color)
+		term.setTextColor(text_color)
+  		term.setBackgroundColor(bg_color)
+  		term.setCursorPos(x,y)
+  		write(text)
+	end
+
+	-- draw a line on the terminal screen
+	function draw_line_term(x, y, length, color)
+		term.setBackgroundColor(color)
+    		term.setCursorPos(x,y)
+    		term.write(string.rep(" ", length))
+	end
+
+	function bars()
+		draw_line_term(1, 1, 51, colors.lime)
+		draw_line_term(1, 19, 51, colors.lime)
+		draw_text_term(15, 1, 'CraftNanny Fluid Module', colors.gray, colors.lime)
+		draw_text_term(10, 19, 'www.craftnanny.org', colors.gray, colors.lime)
+	end
+
+	function terminal_screen()
+		term.clear()
+	
+		bars()
+		draw_text_term(1, 2, 'Module: ', colors.lime, colors.black)
+		draw_text_term(10, 2, module_name, colors.white, colors.black)
+		draw_text_term(1, 3, 'Owner: ', colors.lime, colors.black)
+		draw_text_term(8, 3, username, colors.white, colors.black)
+		draw_text_term(1, 4 , string.rep("-", 51), colors.lime, colors.black)
+	
+		--draw_text_term(2, 8, "I dont know what to put here...", colors.white, colors.black)
+	end
+
+	-- retrieves token from local text file
+	function load_config()
+		sr = fs.open("config.txt", "r")
+    		token = sr.readLine()
+		module_name = sr.readLine()
+		username = sr.readLine()
+		type = sr.readLine()
+  		sr.close()
+	end
+
+	-- called for new installations and when the scanner needs to be updated
+	function run_installer()
+		if fs.exists("install") then
+	    		fs.delete("install")
+	  	end
+	  	shell.run("pastebin get "..installer.." install")
+	  	sleep(1)
+	  	shell.run("install")
+	end
+
+
  
  
         ------  Start module specific code ---------
+
+
+	------ TANK Class, lets do it oop style baby --------
+
+	Tank = {
+		name="", fluidName="Nothing", fluidAmount=0, peripheral="", fluidCapacity=0, fluidPercent=0
+	}
+
+	function Tank:new (o)
+      		o = o or {}   -- create object if user does not provide one
+      		setmetatable(o, self)
+      		self.__index = self
+     		return o
+	end
+	function Tank:calcPercent()
+		if self.fluidAmount > 0 then
+			return round((self.fluidAmount/self.fluidCapacity*100),2)
+		else
+			return 0
+		end
+	end	
+
+	------ // Tank Class -------
  
- 
-        function phone_home(tank_name, fluid_type, percent)
-                response = http.post("http://craftnanny.org/code/fluid.php", "token="..token.."&id="..os.getComputerID().."&tank_name="..tank_name.."&fluid_type="..fluid_type.."&percent="..percent)              
+        function phone_home(t)
+                response = http.post("http://craftnanny.org/code/fluid.php", 		"token="..token.."&id="..os.getComputerID().."&tank_name="..t.name.."&fluid_type="..t.fluidName.."&percent="..t.fluidPercent)              
                 return_string = response.readAll()
                
                 if tonumber(return_string) > version then
                                 run_installer()
                 end
         end
+
+	function writeScreen(t, i)
+		print(t.fluidName," :")
+                graphBar= round((((term.getSize()-10)*t.fluidPercent)/100),0)
+		i = i-1
+                if graphBar < 50 then
+                	draw_line_term(10, 6+i*3, graphBar , colors.green)
+                        draw_line_term(10+graphBar,6+i*3,term.getSize()-graphBar-10,colors.red)
+                        draw_text_term(1, 6+i*3, t.fluidPercent.." % ",colors.lime,colors.black)
+                        term.setBackgroundColor(colors.black)
+                else
+                        draw_line_term(10, 6+i*3, graphBar-10 , colors.green)
+                        draw_text_term(1, 6+i*3, t.fluidPercent.." % ",colors.lime,colors.black)
+                        term.setBackgroundColor(colors.black)
+                end
+		print(" ")print(" ")        
+	end
  
  
         --functions
         function findSide()
-		-- TODO Check if the Peripheral is actually a Tank
+		r = {}
                 for face = 1, 6, 1 do
                         -- Check Sides for normal Tank
                         local f = rs.getSides()[face]
                         if peripheral.isPresent(f) then
-                                return true, f
+                                table.insert(r, f)
                         end
                 end
+		if #r > 0 then
+			return true, r
+		end
                 return false, ""
         end
  
         function round(num, idp)
-          local mult = 10^(idp or 0)
-          return math.floor(num * mult + 0.5) / mult
+          	local mult = 10^(idp or 0)
+          	return math.floor(num * mult + 0.5) / mult
         end
  
-        function getTankInformation(t,tankName)
-                        local tnk=peripheral.wrap(t)
-                        local tankContentAmount = 0
+        function getTankInformation(face,tankName, i)
+		local t= Tank:new{}
+                local tnk=peripheral.wrap(face)
+                local tankContentAmount = 0
+		--t[i] = Tank:new{}
+		t.name = tankName
                        
+			-- get tank information			
+
                         if tnk.isConnected then
-                                                        --is PressurePipes Tank
-                                if(tnk.hasFluid()) then
-                                        local fluid = tnk.getFluid()
-                                        tankContentName = fluid["name"]
-                                        tankContentAmount = fluid["amount"]
+				--is PressurePipes Tank
+
+                                t.fluidCapacity 	= tnk.getCapacity()
+
+				if(tnk.hasFluid()) then
+                                        local fluid 	= tnk.getFluid()
+					
+					t.fluidAmount 	= fluid["amount"]
+					t.fluidName 	= fluid["name"]
+					t.fluidPercent	= t:calcPercent()
                                 else
-                                        tankContentName = "empty"
-                                        tankContentAmount = 0
-                                end
-                                tankCapacity = tnk.getCapacity()
-                                                               
+                                        t.fluidName	= "nothing"
+                                        t.Amount	= 0
+					
+                                end                                								
                         else
-                                tankTbl=tnk.getTankInfo()
-                                tankCapacity=tankTbl[1].capacity
-                                contentsTbl=tankTbl[1].contents or {["rawName"]="nothing",["amount"]=0}
-                                tankContentName=contentsTbl.rawName
-                                tankContentAmount=contentsTbl.amount          
+				okLiquid,msg = pcall(tnk.getTankInfo)
+                                if okLiquid then
+                                	tankTbl		= tnk.getTankInfo()
+                                	t.fluidCapacity	= tankTbl[1].capacity
+                                	contentsTbl	= tankTbl[1].contents or {["rawName"]="nothing",["amount"]=0}
+                                	t.fluidName	= contentsTbl.rawName
+                                	t.fluidAmount	= contentsTbl.amount    
+					t.fluidPercent	= t:calcPercent()      
+				end
                         end
                        
-                        if tankContentAmount then
-                                percent=round((tankContentAmount/tankCapacity*100),2)  
-                        else
-                                percent = 0
-                        end
-                       
-                        --print(tankName," ",tankContentName," ",percent," %        ")
-                        phone_home(tankName, tankContentName, percent)
+			-- process tank information
+			writeScreen(t, i)
+                        phone_home(t)
+			--print(t.name," ",t.fluidName," ",t.fluidPercent," %",t.fluidCapacity)			
+			
+			
+			return true
         end
  
         function notanks()
@@ -143,37 +209,33 @@
         end
  
  
-        function start_loop()
-                        local ok,side = findSide()
-                        if not ok then
-                notanks()
-                                return false
-            end
-               
-                tanks = peripheral.getNames()
- 
-                while true do
-                        terminal_screen()
-                       
-                        -- I'm not use yet how to record multible tanks from one token in the db
-                        -- just recording the first one it connects to for now
-                       
-                        --if #tanks >1 then
-                                -- for tankNo,tank in pairs(tanks) do
-                                --      if tank~=side then
-                                --      getTank(tank,tank)                    
-                                --      end
-               
-                                -- end
-                               
-                        --else
-                                getTankInformation(side,tanks[1])
-                        --end
-                       
-                        -- main active status with server
-                        sleep(30)
-                end
-        end
+	function start_loop()
+		ok,side=findSide ()
+		if not ok then 
+			print("No tank storage found")
+		else
+			tanks = peripheral.getNames()
+			terminal_screen()
+
+			while true do
+				if #side > 0 then
+					for i = 1, #side, 1 do
+						ok = getTankInformation(side[i],"Tank"..os.getComputerID(), i)
+						if not ok then 
+							print("No tank storage found")
+							print("Do you have the right module?")
+							print("Please check your modems")
+							break
+						end
+					end
+			
+				end
+				-- main active status with server
+				sleep(30)
+				return true
+			end
+		end
+	end
  
         function start()
                 term.clear()
